7c50baa0163de9d72894e115d834ef68eb55c30f
arm-mahesh <armanyamaheshwari@gmail.com>
2023-08-01 17:50:00 -0500

    edited webscraper for all dining courts

--- webscraping/webscraping.py
+++ webscraping/webscraping.py
@@ -10,10 +10,14 @@ options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google
 chrome_driver_binary = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
  
-url = 'https://dining.purdue.edu/menus/Ford/2023/8/1/' 
+foodLocations = ["Ford", "Earhart", "Hillenbrand", "Wiley", "Windsor", 
+                 "1Bowl", "Pete's%20Za", "The%20Burrow", "The%20Gathering%20Place", 
+                 "Earhart%20On-the-GO!", "Hillenbrand%20On-the-GO!", "Wiley%20On-the-GO!", "Windsor%20On-the-GO!"]
+ 
+for location in foodLocations:
+    url = 'https://dining.purdue.edu/menus/' + location + '/2023/8/1/' 
+    driver.get(url) 
+    elements = driver.find_elements(By.CLASS_NAME, 'station-item-text') 
+    for element in elements:
+        print(element.text)
 
-driver.get(url) 
-
-elements = driver.find_elements(By.CLASS_NAME, 'station-item-text') 
-for element in elements:
-    print(element.text)
