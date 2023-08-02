4a57e2b3b05f8354ae07ba7373c7487b359a6a5d
arm-mahesh <armanyamaheshwari@gmail.com>
2023-08-01 20:01:57 -0500

    new dish schema

--- src/server/models/Dish.js
+++ src/server/models/Dish.js
@@ -2,26 +2,29 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const dishSchema = new Schema(
-  {
-    nameOfFood: {
+const dishSchema = new Schema({
+  dish: {
+    type: String,
+    required: true,
+  },
+  diningCourt: {
+    type: String,
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  numRatings: {
+    type: Number,
+    default: 0,
+  },
+  daysServed: [
+    {
       type: String,
-      required: true,
-    },
-    diningCourt: {
-      type: String,
-      required: true,
-    },
-    averageRating: { 
-        type: Number, 
-        default: 0 
-    },
-    numberOfRatings: {
-        type: Number,
-        default: 0
-    },
-  }
-);
+    }
+  ]
+});
 
 const Dish = mongoose.model("Dish", dishSchema);
 
