630e63863b284d2a722367250a45b5550e106b7c
Aaditya Doiphode <aadityad27@gmail.com>
2023-08-02 20:24:44 -0700

    basic styling added without bootstrap

--- src/app/components/Rating.jsx
+++ src/app/components/Rating.jsx
@@ -1,18 +1,19 @@
+import "./Rating.css";
+
 export default function Rating(props) {
-  
-   useEffect(() => {
-    const fetchAvgRating = async () => {
-      const response = await fetch("http://localhost:4000/api/dishes", {});
-      const json = await response.json();
-      if (response.ok) {
-        
-      }
-    }
-    fetchAvgRating();
-  }, []);
+  //  useEffect(() => {
+  //   const fetchAvgRating = async () => {
+  //     const response = await fetch("http://localhost:4000/api/dishes", {});
+  //     const json = await response.json();
+  //     if (response.ok) {
+
+  //     }
+  //   }
+  //   fetchAvgRating();
+  // }, []);
 
   return (
-    <div>
+    <div className="bm-cont">
       <img className="boilermaker" src="src/app/assets/boilermaker.png" />
       <img className="boilermaker" src="src/app/assets/boilermaker.png" />
       <img className="boilermaker" src="src/app/assets/boilermaker.png" />
--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
           <Card diningCourt="windsor" />
         </Link>
       </div>
-      <div className="bg-black w-full h-full"></div>
+      {/* <div className="bg-black w-full h-full"></div> */}
     </div>
   );
 }
