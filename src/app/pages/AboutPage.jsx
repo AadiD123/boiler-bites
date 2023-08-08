3b454c7df4d3512ae94f2d14db40074464e24691
Aaditya Doiphode <aadityad27@gmail.com>
2023-08-08 14:52:47 -0700

    fixed css, changed font

--- src/app/pages/AboutPage.jsx
+++ src/app/pages/AboutPage.jsx
@@ -6,17 +6,17 @@ export default function AboutPage() {
         <h3 style={{ textAlign: "center" }}>About</h3>
         <br />
         <p>
-          We made this website to help boilermakers answer the daily struggle of
-          deciding where to eat. Rate your favorite dishes and see what's
-          trending on campus!
+          We made this website to help boilermakers through the daily struggle
+          of deciding where to eat. Use this website to rate your favorite
+          dishes and see what's trending on campus!
         </p>
         <br />
         <p>
-          We want to create all encompassing food hub for Purdue students that
-          would be completely driven by students feedback. We hope that you
-          never have to eat a bad meal again. Reach out to adoiphod@purdue.edu
-          or mahesh43@purdue.edu for feedback and suggestions of features we
-          should implement.
+          We want to create an all-encompassing food hub for Purdue students
+          that would be completely driven by students feedback. We hope that you
+          never have to eat a bad meal again. Feel free to reach out to
+          adoiphod@purdue.edu or mahesh43@purdue.edu for feedback and
+          suggestions of features we should implement.
         </p>
         <br />
         <br />
