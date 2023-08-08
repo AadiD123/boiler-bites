7ec710213c988ee0e0e329aeece216fc312fa99c
arm-mahesh <armanyamaheshwari@gmail.com>
2023-08-08 17:43:58 -0500

    functional calender

--- src/app/pages/DiningCourtPage.jsx
+++ src/app/pages/DiningCourtPage.jsx
@@ -3,25 +3,28 @@ import FoodCard from "../components/FoodCard";
 import { useEffect, useState } from "react";
 import Dish from "../components/Dish";
 import Dropdown from "../components/Dropdown";
+import DatePicker from "../components/DatePicker";
 
 export default function DiningCourtPage(props) {
   // const [dishes, setDishes] = useState([]);
   const [meal, setMeal] = useState("Breakfast");
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [dishesByStation, setDishesByStation] = useState({});
 
-  var currentDate = new Date();
-
-  if (currentDate.getHours() >= 21) {
-    currentDate.setDate(currentDate.getDate() + 1);
+  if (selectedDate.getHours() >= 21) {
+    setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() + 1)));
   }
+  // if (currentDate.getHours() >= 21) {
+  //   currentDate.setDate(currentDate.getDate() + 1);
+  // }
 
   useEffect(() => {
     if (meal !== "") {
       const fetchCurrentFood = async () => {
         const response = await fetch(
-          `http://localhost:4000/api/timings/${currentDate.getFullYear()}/${
-            currentDate.getMonth() + 1
-          }/${currentDate.getDate()}/${props.diningCourt}/${meal}`
+          `http://localhost:4000/api/timings/${selectedDate.getFullYear()}/${
+            selectedDate.getMonth() + 1
+          }/${selectedDate.getDate()}/${props.diningCourt}/${meal}`
         );
 
         if (response.ok) {
@@ -44,6 +47,10 @@ export default function DiningCourtPage(props) {
               groupedDishes[station].push(dish);
             });
 
+            for (const station in groupedDishes) {
+              groupedDishes[station].sort((a, b) => b.averagerating - a.averagerating);
+            }
+
             setDishesByStation(groupedDishes);
           }
         } else {
@@ -55,10 +62,14 @@ export default function DiningCourtPage(props) {
       };
       fetchCurrentFood();
     }
-  }, [meal]);
+  }, [meal, selectedDate]);
 
   const handleMealChange = (selectedMeal) => {
     setMeal(selectedMeal); // Update the meal state
+  };
+
+  const handleDateChange = (selectedDate) => {
+    setSelectedDate(selectedDate); // Update the date state
   };
 
   return (
@@ -67,6 +78,7 @@ export default function DiningCourtPage(props) {
 
       <Dropdown onMealChange={handleMealChange} />
 
+      <DatePicker onSelectDate={handleDateChange}/>
       {Object.keys(dishesByStation).length > 0 ? (
         Object.keys(dishesByStation)
           .sort((stationA, stationB) => {
@@ -78,15 +90,19 @@ export default function DiningCourtPage(props) {
           .map((station) => (
             <div key={station} className="diningCourt-food">
               <h3>{station}</h3>
-              {dishesByStation[station].map((dish) => (
-                <Dish
-                  key={dish._id}
-                  id={dish._id}
-                  dish={dish.dish}
-                  num={dish.numRatings}
-                  avg={dish.averageRating}
-                />
-              ))}
+              {dishesByStation[station]
+                .sort((dishA, dishB) => {
+                  return dishB.averageRating - dishA.averageRating; // Sort by average rating
+                })
+                .map((dish) => (
+                  <Dish
+                    key={dish._id}
+                    id={dish._id}
+                    dish={dish.dish}
+                    num={dish.numRatings}
+                    avg={dish.averageRating}
+                  />
+                ))}
             </div>
           ))
       ) : (
