443861d452aabb47b380261e74dbeb33710e7d38
Aaditya Doiphode <aadityad27@gmail.com>
2023-08-08 14:29:35 -0700

    added about page content

--- src/app/pages/AboutPage.jsx
+++ src/app/pages/AboutPage.jsx
@@ -3,13 +3,24 @@ export default function AboutPage() {
   return (
     <div className="abt-cont">
       <div className="abt-card">
-        <h3>About</h3>
-        {/* <p>
-          We made this website to help fellow boilermakers decide on where to
-          eat on campus. We all face the struggle of which food court looks the
-          best.
-        </p> */}
-        <p>Made by Aaditya Doiphode and Armanya Maheshwari</p>
+        <h3 style={{ textAlign: "center" }}>About</h3>
+        <br />
+        <p>
+          We made this website to help boilermakers answer the daily struggle of
+          deciding where to eat. Rate your favorite dishes and see what's
+          trending on campus!
+        </p>
+        <br />
+        <p>
+          We want to create all encompassing food hub for Purdue students that
+          would be completely driven by students feedback. We hope that you
+          never have to eat a bad meal again. Reach out to adoiphod@purdue.edu
+          or mahesh43@purdue.edu for feedback and suggestions of features we
+          should implement.
+        </p>
+        <br />
+        <br />
+        <p>@ Aaditya Doiphode & Armanya Maheshwari</p>
       </div>
     </div>
   );
