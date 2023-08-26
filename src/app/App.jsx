27bad374fd6f5e0e83d5648d2229d160de5e90ff
Aaditya Doiphode <aadityad27@gmail.com>
2023-08-26 17:16:46 -0400

    fixed 1Bowl

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,47 +7,52 @@ import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <Router>
-      <img className="background-img" src="/assets/purdue.jpg" />
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route
-          path="/earhart"
-          element={<DiningCourtPage diningCourt="Earhart" />}
-        />
-        <Route path="/ford" element={<DiningCourtPage diningCourt="Ford" />} />
-        <Route
-          path="/hillenbrand"
-          element={<DiningCourtPage diningCourt="Hillenbrand" />}
-        />
-        <Route
-          path="/wiley"
-          element={<DiningCourtPage diningCourt="Wiley" />}
-        />
-        <Route
-          path="/windsor"
-          element={<DiningCourtPage diningCourt="Windsor" />}
-        />
-        <Route
-          path="/1bowl"
-          element={<DiningCourtPage diningCourt="1bowl" />}
-        />
-        <Route
-          path="/petesza"
-          element={<DiningCourtPage diningCourt="petes za" />}
-        />
-        <Route
-          path="/theburrow"
-          element={<DiningCourtPage diningCourt="the burrow" />}
-        />
-        <Route
-          path="/thegatheringplace"
-          element={<DiningCourtPage diningCourt="the gathering place" />}
-        />
-        <Route path="/About" element={<AboutPage />} />
-      </Routes>
-    </Router>
+    <div style={{ paddingTop: "env(safe-area-inset-top)" }}>
+      <Router>
+        <img className="background-img" src="/assets/purdue.jpg" />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route
+            path="/earhart"
+            element={<DiningCourtPage diningCourt="Earhart" />}
+          />
+          <Route
+            path="/ford"
+            element={<DiningCourtPage diningCourt="Ford" />}
+          />
+          <Route
+            path="/hillenbrand"
+            element={<DiningCourtPage diningCourt="Hillenbrand" />}
+          />
+          <Route
+            path="/wiley"
+            element={<DiningCourtPage diningCourt="Wiley" />}
+          />
+          <Route
+            path="/windsor"
+            element={<DiningCourtPage diningCourt="Windsor" />}
+          />
+          <Route
+            path="/1bowl"
+            element={<DiningCourtPage diningCourt="1Bowl" />}
+          />
+          <Route
+            path="/petesza"
+            element={<DiningCourtPage diningCourt="Pete's Za" />}
+          />
+          <Route
+            path="/theburrow"
+            element={<DiningCourtPage diningCourt="the burrow" />}
+          />
+          <Route
+            path="/thegatheringplace"
+            element={<DiningCourtPage diningCourt="The%20Gathering%20Place" />}
+          />
+          <Route path="/About" element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
